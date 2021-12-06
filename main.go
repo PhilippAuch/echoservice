9a31571d70a41714f2f3f5a23d7952cb2652a50b
Philipp Auch <philipp.auch@web.de>
2021-12-06 17:55:35 +0100

    attempt at map

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "fmt"
-    "time"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/hello/:name?", func(c *fiber.Ctx) error {
-        name := c.Params("name")
-        fmt.Println(time.Now().Format(time.RFC3339) + " ENTER / GET arguments: name=" + name)
-        if len(name) <= 0 {
-            name = "World"
-        }
+	m := make(map[string]string)
+	m["init"] = "init response"
+	m["second"] = "5"
 
-        fmt.Println(time.Now().Format(time.RFC3339) + " EXIT  / GET arguments: name=" + name)
-        return c.SendString("Hello " + name)
+	app.Get("/hello/:name?", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		fmt.Println(time.Now().Format(time.RFC3339) + " ENTER /hello GET arguments: name=" + name)
+		if len(name) <= 0 {
+			name = "World"
+		}
+
+		fmt.Println(time.Now().Format(time.RFC3339) + " EXIT  /hello GET arguments: name=" + name)
+		return c.SendString("Hello " + name)
 	})
 
-    app.Get("/Joke", func(c *fiber.Ctx) error {
-        
-        return c.SendString("Done")
-    })
+	app.Get("/:key?", func(c *fiber.Ctx) error {
+		key := c.Params("name")
+		fmt.Println(time.Now().Format(time.RFC3339) + " ENTER / GET arguments: key=" + key)
+
+		fmt.Println(time.Now().Format(time.RFC3339)+" map:", m)
+
+		value := ""
+
+		if len(key) > 0 {
+			valueString, _ := json.Marshal(m[key])
+			value = string(valueString)
+		} else {
+			valueString, _ := json.Marshal(m)
+			value = string(valueString)
+		}
+
+		fmt.Println(time.Now().Format(time.RFC3339)+" EXIT  / GET arguments: key="+key+", value=", value)
+		return c.SendString(value)
+	})
 
 	app.Listen(":8080")
 }
